c431ad76c862051a9ac15223a56e570cb07b5720
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-09-26 08:58:43 +0530

    add staff in both db

--- src/models/Staff.js
+++ src/models/Staff.js
@@ -4,7 +4,12 @@ const staffSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   salary: { type: Number, required: true },
   joiningDate: { type: Date, default: Date.now },
-  department: { type: String },
+  department: [
+    {
+      id: { type: Number },
+      name: { type: String }
+    }
+  ],
 });
 
 module.exports = mongoose.model("Staff", staffSchema);
