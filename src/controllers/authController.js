6ebc06d9265480f8579198882139f730b8765578
Sayain-Aditya <as4316664@gmail.com>
2025-11-04 18:01:16 +0530

    asd

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -76,13 +76,12 @@ exports.register = async (req, res) => {
       if (salaryDetails) userData.salaryDetails = salaryDetails;
     } else if (role === "admin") {
       userData.department = [
-        { id: 1, name: "kitchen" },
-        { id: 2, name: "laundry" },
-        { id: 3, name: "reception" },
-        { id: 4, name: "maintenance" },
-        { id: 5, name: "other" },
-        { id: 6, name: "housekeeping" },
-        { id: 7, name: "pantry" },
+        { id: 1, name: "laundry" },
+        { id: 2, name: "reception" },
+        { id: 3, name: "maintenance" },
+        { id: 4, name: "other" },
+        { id: 5, name: "housekeeping" },
+        { id: 6, name: "pantry" },
       ];
     } else if (role === "restaurant") {
       userData.restaurantRole = restaurantRole;
