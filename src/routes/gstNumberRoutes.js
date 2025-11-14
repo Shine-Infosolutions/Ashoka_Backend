1c6c98c1a22769fdd795f57a0f6a1629856c6648
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-11-14 17:11:26 +0530

    gg

--- src/routes/gstNumberRoutes.js
+++ src/routes/gstNumberRoutes.js
@@ -4,13 +4,15 @@ const {
   createGSTNumber, 
   getAllGSTNumbers, 
   getGSTNumberById, 
+  getGSTDetails,
   updateGSTNumber, 
   deleteGSTNumber 
 } = require('../controllers/gstNumberController');
 
-// GST Number routes
+// GST Number routes (Customer/Company details)
 router.post('/create', createGSTNumber);
 router.get('/all', getAllGSTNumbers);
+router.get('/details/:gstNumber', getGSTDetails);
 router.get('/:id', getGSTNumberById);
 router.put('/update/:id', updateGSTNumber);
 router.delete('/delete/:id', deleteGSTNumber);
