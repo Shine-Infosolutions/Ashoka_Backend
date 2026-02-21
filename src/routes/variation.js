3b1250aeaf3573656344d80ae3ba00854331530d
Sayain-Aditya <as4316664@gmail.com>
2026-02-21 15:25:19 +0530

    cloudnary remove

--- src/routes/variation.js
+++ src/routes/variation.js
@@ -6,13 +6,14 @@ const {
     updateVariation,
     deleteVariation
 } = require('../controllers/variationController');
+const { auth, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/add/variation', createVariation);
-router.get('/all/variation', getVariations);
-router.get('/get/variation/:id', getVariationById);
-router.put('/update/variation/:id', updateVariation);
-router.delete('/delete/variation/:id', deleteVariation);
+router.post('/add/variation', auth, authorize(['ADMIN', 'STAFF']), createVariation);
+router.get('/all/variation', auth, authorize(['ADMIN', 'STAFF', 'FRONT DESK']), getVariations);
+router.get('/get/variation/:id', auth, authorize(['ADMIN', 'STAFF', 'FRONT DESK']), getVariationById);
+router.put('/update/variation/:id', auth, authorize(['ADMIN', 'STAFF']), updateVariation);
+router.delete('/delete/variation/:id', auth, authorize(['ADMIN']), deleteVariation);
 
 module.exports = router;
